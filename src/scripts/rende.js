1943acffb609ec82636cd7df75495e8c0dba389c
wdfwwkkee <denis.usmanov101@mail.ru>
2024-01-18 17:06:48 +0500

    fix

--- src/scripts/rende.js
+++ src/scripts/rende.js
@@ -2,7 +2,7 @@
 
 
 async function render() {
-    const responce = await fetch('https://fakestoreapi.com/products');
+    const responce = await fetch('https://my-json-server.typicode.com/wdfwwkkee/tortiki/db');
     const array = await responce.json();
     console.log(array)
 }
